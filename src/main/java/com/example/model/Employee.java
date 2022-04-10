3b91e14ea459bc67b72158a610c440b6064a35dd
Nitin Gorte <nitin.gorte005@gmail.com>
2022-04-10 18:33:14 +0530

    param construct added

--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -1,44 +1,31 @@
 package com.example.model;
 
+
 public class Employee {
 
 	private int EmployeeId;
 	private String EmployeeName;
-	private String Dept;
-
+	private String dept;
 	public Employee() {
 		super();
 	}
-
-	public Employee(int employeeId, String employeeName, String dept) {
-		super();
-		EmployeeId = employeeId;
-		EmployeeName = employeeName;
-		Dept = dept;
-	}
-
 	public int getEmployeeId() {
 		return EmployeeId;
 	}
-
 	public void setEmployeeId(int employeeId) {
 		EmployeeId = employeeId;
 	}
-
 	public String getEmployeeName() {
 		return EmployeeName;
 	}
-
 	public void setEmployeeName(String employeeName) {
 		EmployeeName = employeeName;
 	}
-
 	public String getDept() {
-		return Dept;
+		return dept;
 	}
-
 	public void setDept(String dept) {
-		Dept = dept;
+		this.dept = dept;
 	}
-
+	
 }
