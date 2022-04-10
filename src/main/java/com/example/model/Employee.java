b016102fb2293de28c2bd58e28cb5abe77f55bc1
Nitin Gorte <nitin.gorte005@gmail.com>
2022-04-10 18:27:02 +0530

    employee model updated with new fields

--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -1,8 +1,44 @@
 package com.example.model;
 
-
 public class Employee {
 
 	private int EmployeeId;
-	
+	private String EmployeeName;
+	private String Dept;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(int employeeId, String employeeName, String dept) {
+		super();
+		EmployeeId = employeeId;
+		EmployeeName = employeeName;
+		Dept = dept;
+	}
+
+	public int getEmployeeId() {
+		return EmployeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		EmployeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return EmployeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		EmployeeName = employeeName;
+	}
+
+	public String getDept() {
+		return Dept;
+	}
+
+	public void setDept(String dept) {
+		Dept = dept;
+	}
+
 }
